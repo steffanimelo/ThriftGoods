5cbf0a1956f36980d5a51d2ecfe13c7bd0ff5528
Steffani <sartinisteffani@gmail.com>
2022-06-12 20:42:00 +0200

    changing to files + debugging

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let x = 1;
+  console.log('App executed');
+
+  return (
+    <View style={styles.container}>
+      <Text>Heeey yo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# ThriftGoods
+React Native App for Second hand products
+## In this app, I'm using Expo CLI as the first React Native Application during this adventure of learning React Native. 
